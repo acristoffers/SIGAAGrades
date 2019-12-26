e668d6054ba3186616a6c1cb1eafdd79db851495
Álan Crístoffer <acristoffers@gmail.com>
2019-12-26 11:48:48 -0300

    Adds macOS support.

---
+++ Flutter/sigaa_notas/android/app/src/main/kotlin/sigaa/acristoffers/me/sigaa_notas/MainActivity.kt
@@ -0,0 +1,12 @@
+package sigaa.acristoffers.me.sigaa_notas
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
