294f441672ecd0e9d72fa54c59e0fee94304ab87
Álan Crístoffer <alan@MacBook-Pro-de-Alan.local>
2020-07-26 14:03:39 -0300

    Upgrades Flutter.

--- Flutter/sigaa_notas/macos/Flutter/GeneratedPluginRegistrant.swift
+++ Flutter/sigaa_notas/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,11 +5,13 @@
 import FlutterMacOS
 import Foundation
 
+import package_info
 import shared_preferences_macos
 import sqflite
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTPackageInfoPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
